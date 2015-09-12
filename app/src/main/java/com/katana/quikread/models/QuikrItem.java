73d233ad9dc234b926619922154d7658ff1b87c5
Akshay Deshraj <akshay30nov@gmail.com>
2015-09-12 07:13:19 +0530

    create model for quikr item

---
+++ app/src/main/java/com/katana/quikread/models/QuikrItem.java
@@ -0,0 +1,31 @@
+package com.katana.quikread.models;
+
+/**
+ * @author Akshay
+ * @version 1.0.0
+ * @since 12-Sep-15
+ */
+public class QuikrItem {
+
+    private String attributeAdType;
+
+    private String cityName;
+
+    private String metaCategoryName;
+
+    private String adLocality;
+
+    private String content;
+
+    private String images;
+
+    private String id;
+
+    private String title;
+
+    private String attributeGenre;
+
+    private String url;
+
+    private String geoPin; //TODO : maybe show on map
+}
