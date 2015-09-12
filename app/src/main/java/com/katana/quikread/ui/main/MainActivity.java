6a68d1bf24ed1e9aee6735a95fe3c61dbc6c3e94
Akshay Deshraj <akshay30nov@gmail.com>
2015-09-12 23:39:55 +0530

    corrections in QuikrItem

--- app/src/main/java/com/katana/quikread/ui/main/MainActivity.java
+++ app/src/main/java/com/katana/quikread/ui/main/MainActivity.java
@@ -21,6 +21,7 @@ import javax.inject.Inject;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import dagger.Component;
+import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
@@ -56,8 +57,18 @@ public class MainActivity extends BaseActivity implements OnRequestFinishedListe
 
         restDataSource.setOnRequestFinishedListener(this);
 
-        //restDataSource.fetchBooksByLocation("Delhi"); //TODO: remove hardcoded location
-        restDataSource.searchBookByTitle("Hardy Boys");
+        restDataSource.fetchBooksByLocation("Delhi"); //TODO: remove hardcoded location
+        restDataSource.searchBookByTitle("", new Callback() {
+            @Override
+            public void success(Object o, Response response) {
+
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
     }
 
     @Override
