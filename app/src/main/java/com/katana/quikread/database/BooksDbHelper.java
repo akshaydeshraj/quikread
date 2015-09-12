291546c7ff124743a5f46a1fc23e9d6e36e44140
Akshay Deshraj <akshay30nov@gmail.com>
2015-09-12 15:48:28 +0530

    rename class and fix minor typo

--- app/src/main/java/com/katana/quikread/database/BooksDbHelper.java
+++ app/src/main/java/com/katana/quikread/database/BooksDbHelper.java
@@ -5,9 +5,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import javax.inject.Singleton;
-
-public class BooksDataBase extends SQLiteOpenHelper {
+public class BooksDbHelper extends SQLiteOpenHelper {
 
     //Database version
     private static final int DATABASE_VERSION = 1;
@@ -27,12 +25,12 @@ public class BooksDataBase extends SQLiteOpenHelper {
     private static final String IMAGE_URL = "image_url";
     private static final String BOOK_ID = "id";
     private static final String BOOK_TITLE = "title";
-    private static final String GENERE = "genere";
+    private static final String GENRE = "genre";
     private static final String ADD_URL = "add_url";
     private static final String GEO_PIN = "geo_pin";
 
 
-    public BooksDataBase(Context context) {
+    public BooksDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
@@ -45,7 +43,7 @@ public class BooksDataBase extends SQLiteOpenHelper {
                 " ("+ KEY_ID +" integer primary key AUTOINCREMENT, " +
                 ADD_TYPE+" text," + CITY_NAME+" text, "+AD_LOCALITY +
                 " text,"+ CONTENT+" text, " + IMAGE_URL+" text,"+ BOOK_ID+" text, "
-                +BOOK_TITLE+" text," + GENERE+" text,"+ ADD_URL+" text, "+GEO_PIN+" text)";
+                +BOOK_TITLE+" text," + GENRE +" text,"+ ADD_URL+" text, "+GEO_PIN+" text)";
 
         db.execSQL(createTable);
     }
@@ -59,7 +57,7 @@ public class BooksDataBase extends SQLiteOpenHelper {
 
     public String[] getTableColumns(){
         String[] array = {KEY_ID,ADD_TYPE,CITY_NAME,AD_LOCALITY,CONTENT,IMAGE_URL,BOOK_ID
-                ,BOOK_TITLE,GENERE,ADD_URL,GEO_PIN};
+                ,BOOK_TITLE, GENRE,ADD_URL,GEO_PIN};
         return array ;
     }
 
