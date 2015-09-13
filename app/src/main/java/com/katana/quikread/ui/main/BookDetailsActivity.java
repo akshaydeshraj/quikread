413f62c955f383261d5b1259fb986db5377f0019
Bhupendra Singh Chauhan <birendrachauhan3@gmail.com>
2015-09-14 00:04:37 +0530

    changes for map BookDetai

--- app/src/main/java/com/katana/quikread/ui/main/BookDetailsActivity.java
+++ app/src/main/java/com/katana/quikread/ui/main/BookDetailsActivity.java
@@ -1,11 +1,20 @@
 package com.katana.quikread.ui.main;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.github.polok.routedrawer.RouteDrawer;
+import com.github.polok.routedrawer.RouteRest;
+import com.github.polok.routedrawer.model.Routes;
+import com.github.polok.routedrawer.model.TravelMode;
+import com.github.polok.routedrawer.parser.RouteJsonParser;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
 import com.katana.quikread.R;
 import com.katana.quikread.models.QuikreadItem;
 
@@ -27,6 +36,8 @@ public class BookDetailsActivity extends AppCompatActivity {
     @Bind(R.id.details_content)
     TextView content;
 
+    private GoogleMap googleMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +49,33 @@ public class BookDetailsActivity extends AppCompatActivity {
 
         city.setText(quikreadItem.getCityName());
 
-        content.setText(""+quikreadItem.getQuickrContent());
+        content.setText("" + quikreadItem.getQuickrContent());
+
+        googleMap = ((com.google.android.gms.maps.MapFragment)getFragmentManager().findFragmentById(R.id.details_map)).getMap();
+
+        final RouteDrawer routeDrawer = new RouteDrawer.RouteDrawerBuilder(googleMap)
+                .withColor(Color.BLUE)
+                .withWidth(8)
+                .withAlpha(0.5f)
+                .withMarkerIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+                .build();
+
+        RouteRest routeRest = new RouteRest();
+
+        routeRest.getJsonDirections(new LatLng(50.126922, 19.015261), new LatLng(50.200206, 19.175603), TravelMode.DRIVING)
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(new Func1<String, Routes>() {
+                    @Override
+                    public Routes call(String s) {
+                        return new RouteJsonParser<Routes>().parse(s, Routes.class);
+                    }
+                })
+                .subscribe(new Action1<Routes>() {
+                    @Override
+                    public void call(Routes r) {
+                        routeDrawer.drawPath(r);
+                    }
+                });
 
     }
 
