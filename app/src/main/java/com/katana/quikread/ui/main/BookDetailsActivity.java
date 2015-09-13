40ce5dc49810cdf663f343acd2f83321137f0128
deep-154 <deepankar11093@gmail.com>
2015-09-13 22:51:42 +0530

    map fragment added

--- app/src/main/java/com/katana/quikread/ui/main/BookDetailsActivity.java
+++ app/src/main/java/com/katana/quikread/ui/main/BookDetailsActivity.java
@@ -1,8 +1,6 @@
 package com.katana.quikread.ui.main;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -36,6 +34,8 @@ public class BookDetailsActivity extends AppCompatActivity {
         QuikreadItem quikreadItem = (QuikreadItem)getIntent().getSerializableExtra("quickread");
 
         city.setText(quikreadItem.getCityName());
+
+        locality.setText(""+quikreadItem.getQuickrContent());
 
     }
 
---
+++ app/src/main/java/com/katana/quikread/ui/main/MapFragment.java
@@ -0,0 +1,10 @@
+package com.katana.quikread.ui.main;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @author DEEPANKAR
+ * @since 13-09-2015.
+ */
+public class MapFragment extends Fragment{
+}
