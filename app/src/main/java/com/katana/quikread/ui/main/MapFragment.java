38d1940e393b5658e9c03fada4040b3501a4dbec
Bhupendra Singh Chauhan <birendrachauhan3@gmail.com>
2015-09-13 23:26:09 +0530

    mapfragment modifies

--- app/src/main/java/com/katana/quikread/ui/main/MapFragment.java
+++ app/src/main/java/com/katana/quikread/ui/main/MapFragment.java
@@ -1,10 +1,61 @@
 package com.katana.quikread.ui.main;
 
+import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+
+
+import com.github.polok.routedrawer.RouteDrawer;
+import com.github.polok.routedrawer.RouteRest;
+import com.github.polok.routedrawer.model.Routes;
+import com.github.polok.routedrawer.model.TravelMode;
+import com.github.polok.routedrawer.parser.RouteJsonParser;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.katana.quikread.R;
 
 /**
  * @author DEEPANKAR
  * @since 13-09-2015.
  */
 public class MapFragment extends Fragment{
+
+    private GoogleMap googleMap;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.map_fragment);
+
+        googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        final RouteDrawer routeDrawer = new RouteDrawer.RouteDrawerBuilder(googleMap)
+                .withColor(Color.BLUE)
+                .withWidth(8)
+                .withAlpha(0.5f)
+                .withMarkerIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+                .build();
+
+        RouteRest routeRest = new RouteRest();
+
+        routeRest.getJsonDirections(new LatLng(50.126922, 19.015261), new LatLng(50.200206, 19.175603), TravelMode.DRIVING)
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(new Func1<String, Routes>() {
+                    @Override
+                    public Routes call(String s) {
+                        return new RouteJsonParser<Routes>().parse(s, Routes.class);
+                    }
+                })
+                .subscribe(new Action1<Routes>() {
+                    @Override
+                    public void call(Routes r) {
+                        routeDrawer.drawPath(r);
+                    }
+                });
+
+    }
+
 }
+
